9dbfaba121a6efdb18ea1cd7eef46a7d0f1065f7
Sourabh4896 <pawarsourabh045@gmail.com>
2024-12-01 12:43:49 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Fetch available cameras
+  final cameras = await availableCameras();
+  final firstCamera = cameras.first;
+
+  runApp(MyApp(camera: firstCamera));
+}
+
+class MyApp extends StatelessWidget {
+  final CameraDescription camera;
+
+  const MyApp({Key? key, required this.camera}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CameraScreen(camera: camera),
+    );
+  }
+}
+
+class CameraScreen extends StatefulWidget {
+  final CameraDescription camera;
+
+  const CameraScreen({Key? key, required this.camera}) : super(key: key);
+
+  @override
+  _CameraScreenState createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = CameraController(
+      widget.camera,
+      ResolutionPreset.medium,
+    );
+
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Flutter Camera"),
+        backgroundColor: Colors.teal,
+      ),
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return CameraPreview(_controller);
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          try {
+            await _initializeControllerFuture;
+
+            final image = await _controller.takePicture();
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: Text("Image captured at: ${image.path}"),
+            ));
+          } catch (e) {
+            print(e);
+          }
+        },
+        child: const Icon(Icons.camera),
+      ),
+    );
+  }
+}
